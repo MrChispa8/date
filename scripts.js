d8e66922b1d344322bbb05f46a3cd339d0c520a0
Tom Glz <101127043+MrChispa8@users.noreply.github.com>
2022-03-17 18:19:19 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,51 @@
+/*
+function saludar(nombre){
+    alert("Hola " + nombre);
+}
+
+saludar("Alberto");
+saludar("Violeta");
+saludar("Alfonso");
+saludar("Abbi");
+saludar("Omar");
+function darHora(){
+document.getElementById("d_time").innerHTML = Date();
+}
+*/
+
+const horaYfechaActual = new Date();
+const hora = horaYfechaActual.getHours(); // + ':' + horaYfechaActual.getMinutes();
+const minutos = horaYfechaActual.getMinutes();
+const fecha = horaYfechaActual.getDate();
+
+//console.log(hora);
+var saludo = "";
+if(hora > 0 && hora <= 11){
+    saludo="¡Buenos días!";
+}
+if(hora > 11 && hora <= 18){
+    saludo="¡Buenos tardes!";
+}
+if(hora > 18 && hora <= 0){
+    saludo="¡Buenos noches!";
+}
+
+function darHora(){
+    document.getElementById("d_hora").innerHTML = "Son las " + hora + ":" + minutos;
+}
+function saludar(){
+    document.getElementById("d_saludo").innerHTML = saludo;
+}
+function darFecha(){
+    document.getElementById("d_fecha").innerHTML = fecha;
+}
+
+function borrarHora(){
+    document.getElementById("d_hora").innerHTML = ""; 
+}
+function borrarSaludo(){
+    document.getElementById("d_saludo").innerHTML = ""; 
+}
+function borrarFecha(){
+    document.getElementById("d_fecha").innerHTML = ""; 
+}
